6af940dc17a03634ad7393e8bd295a05e76f00e6
Stevan Goode <steve@madhouseminers.com>
2020-09-13 12:26:54 +0100

    Fixed casing

--- src/events/cheer.ts
+++ src/events/cheer.ts
@@ -3,11 +3,14 @@ import { wsClient } from "..";
 
 export default (channel: string, userstate: Userstate, message: string) => {
   if (wsClient) {
-    let emotes = message.split(" ").map((m) => {
-      const type = m.match(/^[a-zA-Z]+/)[0];
-      const amount = m.match(/[0-9]+$/)[0];
-      return `https://d3aqoihi2n8ty8.cloudfront.net/actions/${type}/dark/animated/${amount}/3.gif`;
-    });
+    let emotes = message
+      .toLowerCase()
+      .split(" ")
+      .map((m) => {
+        const type = m.match(/^[a-zA-Z]+/)[0];
+        const amount = m.match(/[0-9]+$/)[0];
+        return `https://d3aqoihi2n8ty8.cloudfront.net/actions/${type}/dark/animated/${amount}/3.gif`;
+      });
 
     wsClient.send(`CHEER ${userstate.bits} ${emotes[0]}`);
   }
