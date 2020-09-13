b09c32fbc660b680094ec6744eff27c072793aa7
Stevan Goode <steve@madhouseminers.com>
2020-09-13 12:24:38 +0100

    Added gif drop for proper cheer icons

--- src/events/cheer.ts
+++ src/events/cheer.ts
@@ -3,18 +3,11 @@ import { wsClient } from "..";
 
 export default (channel: string, userstate: Userstate, message: string) => {
   if (wsClient) {
-    let emotes = [];
-
-    if (userstate.emotes) {
-      const emoteIds = Object.keys(userstate.emotes);
-      emotes = emoteIds.map(
-        (emote) => `https://static-cdn.jtvnw.net/emoticons/v1/${emote}/3.0`
-      );
-    }
-
-    if (!emotes.length && process.env.DEFAULT_EMOTE) {
-      emotes.push(process.env.DEFAULT_EMOTE);
-    }
+    let emotes = message.split(" ").map((m) => {
+      const type = m.match(/^[a-zA-Z]+/)[0];
+      const amount = m.match(/[0-9]+$/)[0];
+      return `https://d3aqoihi2n8ty8.cloudfront.net/actions/${type}/dark/animated/${amount}/3.gif`;
+    });
 
     wsClient.send(`CHEER ${userstate.bits} ${emotes[0]}`);
   }
